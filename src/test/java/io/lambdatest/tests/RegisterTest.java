509b7d53d71b2d2f4bcf24b6c4a980c99e7b62bd
Przemyslaw Botwina <przemek.botwina@o2.pl>
2024-10-22 09:11:48 +0200

    add BaseTest with setup method

--- src/test/java/io/lambdatest/tests/RegisterTest.java
+++ src/test/java/io/lambdatest/tests/RegisterTest.java
@@ -1,14 +1,11 @@
 package io.lambdatest.tests;
 
-import io.lambdatest.utils.DriverFactory;
-import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
-public class RegisterTest {
+public class RegisterTest extends BaseTest {
 
     @Test
     public void register() {
-        WebDriver driver = DriverFactory.getDriver("chrome");
         driver.get("https://ecommerce-playground.lambdatest.io/");
     }
 }
