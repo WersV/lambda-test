c732536464b5c51692f12d0533f1f45977065b7f
Przemyslaw Botwina <przemek.botwina@o2.pl>
2024-10-22 20:32:31 +0200

    add HomePage and registerTest

--- src/test/java/io/lambdatest/pages/HomePage.java
+++ src/test/java/io/lambdatest/pages/HomePage.java
@@ -1,5 +1,34 @@
 package io.lambdatest.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
 public class HomePage {
 
+    private WebDriver driver;
+
+    public HomePage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+    }
+
+    @FindBy(xpath = "//a[@href='https://ecommerce-playground.lambdatest.io/index.php?route=account/account']")
+    private List<WebElement> myAccBtn;
+
+    @FindBy(xpath = "//a[@href='https://ecommerce-playground.lambdatest.io/index.php?route=account/login']")
+    private WebElement loginLiBtn;
+
+    @FindBy(xpath = "//span[contains(text(), 'Register')]")
+    private WebElement registerBtn;
+
+    public void goToRegisterPage() {
+        Actions actions = new Actions(driver);
+        myAccBtn.stream().filter(el -> el.isDisplayed()).findFirst().ifPresent(el -> actions.moveToElement(el).perform());
+        registerBtn.click();
+    }
 }
--- src/test/java/io/lambdatest/tests/RegisterTest.java
+++ src/test/java/io/lambdatest/tests/RegisterTest.java
@@ -1,5 +1,6 @@
 package io.lambdatest.tests;
 
+import io.lambdatest.pages.HomePage;
 import org.testng.annotations.Test;
 
 public class RegisterTest extends BaseTest {
@@ -7,5 +8,6 @@ public class RegisterTest extends BaseTest {
     @Test
     public void register() {
         driver.get("https://ecommerce-playground.lambdatest.io/");
+        new HomePage(driver).goToRegisterPage();
     }
 }
