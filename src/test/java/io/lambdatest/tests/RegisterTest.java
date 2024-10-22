9c9cc4ed9e9fdc4c009b0d6b966b536b382bff4e
Przemyslaw Botwina <przemek.botwina@o2.pl>
2024-10-22 09:03:10 +0200

    add DriverFactory

--- src/test/java/io/lambdatest/tests/RegisterTest.java
+++ src/test/java/io/lambdatest/tests/RegisterTest.java
@@ -1,5 +1,14 @@
 package io.lambdatest.tests;
 
+import io.lambdatest.utils.DriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
 public class RegisterTest {
 
+    @Test
+    public void register() {
+        WebDriver driver = DriverFactory.getDriver("chrome");
+        driver.get("https://ecommerce-playground.lambdatest.io/");
+    }
 }
